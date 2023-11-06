e1c9c2953919cc5a8bdc329a4b02d12e50294e6a
Mykolas Jerutis <mykolasjerutis@email.com>
2023-11-06 19:22:23 +0200

    Fixed function names

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def adding_nums(first_num:int,second_num:int) -> int:
+def summing_two_nums(first_num:int,second_num:int) -> int:
     return first_num + second_num
-def multiplying_nums(first_num:int,second_num:int) -> int:
+def multiplying_two_nums(first_num:int,second_num:int) -> int:
     return first_num * second_num
