db066edccd1227da3651f1f125fb6ea0d67e59c7
Mykolas Jerutis <mykolasjerutis@email.com>
2023-11-06 19:56:59 +0200

    Modify main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def adding_numbers(first_num:int,second_num:int) -> int:
     return first_num + second_num
-def multiplying_numbers(first_num:int,second_num:int) -> int:
+def multiplying_numbers(first_num:float,second_num:float) -> float:
     return first_num * second_num
+def dividing_two_nums(first_num:int, seconf_num:int) -> int:
+    return round(first_num/seconf_num)
