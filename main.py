f84d8116d60b1bb384ec08e12d82916c44206737
Mindaugas Minelga <minelga.m@gmail.com>
2023-11-06 19:22:30 +0200

    modify main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 def adding_numbers(first_num:int,second_num:int) -> int:
     return first_num + second_num
-
-
-
-
 def multiplying_numbers(first_num:int,second_num:int) -> int:
     return first_num * second_num
