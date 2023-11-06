49629d91cecd51e6b61c1fc69df1fdbe2012f32c
Mykolas Jerutis <mykolasjerutis@email.com>
2023-11-06 19:09:49 +0200

    Add two functions to main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+def adding_nums(first_num:int,second_num:int) -> int:
+    return first_num + second_num
+def multiplying_nums(first_num:int,second_num:int) -> int:
+    return first_num * second_num
